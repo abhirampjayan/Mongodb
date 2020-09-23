8ef006b498b3b277932525d983563e429dbb2876
Abhiram P <abhiram.p@ecstvm.local>
2020-09-23 16:48:27 +0530

    Mongo Db Connected and listed databases

---
+++ src/test/java/org/example/LocalDB.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoIterable;
+
+public class LocalDB {
+    public static void main(String[] args) {
+        MongoClient client=new MongoClient("127.0.0.1",27017);
+        MongoIterable<String> mitems= client.listDatabaseNames();
+        mitems.forEach(new Block<String>() {
+            @Override
+            public void apply(String dbname) {
+                System.out.println(" "+dbname);
+
+            }
+        });
+    }
+}
